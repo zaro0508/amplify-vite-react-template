b673e3e35a0c1adc62b1e693859e4000de49bc6c
Khai Do <zaro0508@gmail.com>
2025-06-21 08:34:19 -0700

    Revert "enable auth"
    This reverts commit b01c3122cdfb3fc281e000fdd9ecd1e0d499f586.

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <App />
   </React.StrictMode>
 );
